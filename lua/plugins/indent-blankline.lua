579f6ecb5bba3770d5e0316f1086f80b81e73a94
aniketmahajan04 <mahajananiket207@gmail.com>
2025-12-04 17:20:19 +0530

    feat: new indent-blankline plugin added with matching theme

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -23,27 +23,32 @@ return {
     },
     -- ... (Add the rainbow highlights back to 'indent')
     indent = {
-      char = "│",
+      char = "▏",
       highlight = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
+        -- "IndentBlanklineIndent1",
+        -- "IndentBlanklineIndent2",
+        -- "IndentBlanklineIndent3",
+        -- "IndentBlanklineIndent4",
+        -- "IndentBlanklineIndent5",
+        -- "IndentBlanklineIndent6",
+        "IblIndent",
       },
     },
     -- ... (Add the scope highlight back to 'scope')
     scope = {
       enabled = true,
+      -- show_start = false,
+      -- show_end = false,
       highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowOrange",
-        "RainbowGreen",
-        "RainbowViolet",
-        "RainbowCyan",
+        -- "RainbowRed",
+        -- "RainbowYellow",
+        -- "RainbowBlue",
+        -- "RainbowOrange",
+        -- "RainbowGreen",
+        -- "RainbowViolet",
+        -- "RainbowCyan",
+
+        "IblScope"
       },
     },
   },
@@ -51,41 +56,47 @@ return {
   -- The config function is executed *after* the plugin is loaded
   config = function(_, opts)
     --1. Define highlights BEFORE registering hooks and setup
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { fg = "#E06C75", blend = 90 })
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { fg = "#E5C07B", blend = 90 })
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent3", { fg = "#98C379", blend = 90 })
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent4", { fg = "#61AFEF", blend = 90 })
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent5", { fg = "#C678DD", blend = 90 })
-    vim.api.nvim_set_hl(0, "IndentBlanklineIndent6", { fg = "#56B6C2", blend = 90 })
+    -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { fg = "#E06C75" })
+    -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { fg = "#E5C07B" })
+    -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent3", { fg = "#98C379" })
+    -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent4", { fg = "#61AFEF" })
+    -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent5", { fg = "#C678DD" })
+    -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent6", { fg = "#56B6C2" })
+    vim.api.nvim_set_hl(0, "IblIndent", { fg = "#4a4e57", nocombine = true })
 
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75", blend = 70 })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B", blend = 70 })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF", blend = 70 })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66", blend = 70 })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379", blend = 70 })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD", blend = 70 })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2", blend = 70 })
+    -- vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    -- vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    -- vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    -- vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    -- vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    -- vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    -- vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    vim.api.nvim_set_hl(0, "IblScope", { fg = "#61afef", nocombine = true })
 
     -- 2. Use the IBL hook to define custom highlight groups
     -- This ensures the highlights are reapplied whenever the colorscheme changes.
     local hooks = require "ibl.hooks"
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       -- Indent line colors (thin lines)
-      vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { fg = "#E06C75", nocombine = true, blend = 90 })
-      vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { fg = "#E5C07B", nocombine = true, blend = 90 })
-      vim.api.nvim_set_hl(0, "IndentBlanklineIndent3", { fg = "#98C379", nocombine = true, blend = 90 })
-      vim.api.nvim_set_hl(0, "IndentBlanklineIndent4", { fg = "#61AFEF", nocombine = true, blend = 90 })
-      vim.api.nvim_set_hl(0, "IndentBlanklineIndent5", { fg = "#C678DD", nocombine = true, blend = 90 })
-      vim.api.nvim_set_hl(0, "IndentBlanklineIndent6", { fg = "#56B6C2", nocombine = true, blend = 90 })
+      -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { fg = "#E06C75", nocombine = true })
+      -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { fg = "#E5C07B", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent3", { fg = "#98C379", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent4", { fg = "#61AFEF", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent5", { fg = "#C678DD", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent6", { fg = "#56B6C2", nocombine = true, })
+      vim.api.nvim_set_hl(0, "IblIndent", { fg = "#4a4e57", nocombine = true })
+
 
       -- Scope/rainbow colors (for current context)
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75", nocombine = true, blend = 70 })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B", nocombine = true, blend = 70 })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF", nocombine = true, blend = 70 })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66", nocombine = true, blend = 70 })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379", nocombine = true, blend = 70 })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD", nocombine = true, blend = 70 })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2", nocombine = true, blend = 70 })
+      -- vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD", nocombine = true, })
+      -- vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2", nocombine = true, })
+
+      vim.api.nvim_set_hl(0, "IblScope", { fg = "#61afef", nocombine = true })
     end)
     require("ibl").setup(opts)
   end
